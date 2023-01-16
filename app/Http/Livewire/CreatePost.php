40ac19a7e57834b0d8e79d7fa83b70f796c80c13
fredinho777 <fredinho777@hotmail.com>
2023-01-16 17:14:17 -0500

    Add files, se crea modulo Post usando componentes de livewire

---
+++ app/Http/Livewire/CreatePost.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Post;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class CreatePost extends Component
+{
+    use WithFileUploads;
+    public $open = true;
+    public $title, $content, $image, $identificador;
+
+    public function mount(){
+        $this->identificador = rand();
+    }
+
+    protected $rules = [
+        'title' => 'required',
+        'content' => 'required',
+        'image' => 'required|image|max:2048'
+    ];
+
+    
+    public function save(){
+
+        $this->validate();
+
+        $image = $this->image->store('posts');
+
+        Post::create([
+            'title' => $this->title,
+            'content'=> $this->content,
+            'image' => $image
+        ]);
+
+        $this->reset(['open', 'title', 'content', 'image']);
+
+        $this->identificador = rand();
+
+        // $this->emit('render');
+        $this->emitTo('show-posts','render');
+        $this->emit('alert', 'El post se creó satisfactoriamente');
+    }
+    public function render()
+    {
+        return view('livewire.create-post');
+    }
+}
---
+++ resources/views/livewire/show-posts.blade.php
@@ -0,0 +1,99 @@
+<div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+	
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <!-- component -->
+    <div class="bg-white p-8 rounded-md w-full">
+    			<div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+    				<div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+						<div class="px-6 py-4 flex items-center">
+							{{-- <input type="text" wire:model="search"> --}}
+							<x-jet-input class="flex-1 mr-4" type="text" placeholder="Buscar" wire:model="search" />
+							@livewire('create-post')
+						</div>
+						@if ($posts->count())
+
+							<table class="min-w-full leading-normal">
+	    						<thead>
+	    							<tr>
+	    								<th class="w-24 cursor-pointer px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider" wire:click="order('id')">
+	    									ID
+
+											@if ($sort == 'id')
+												@if ($direction == 'asc')
+													<i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+												@else
+													<i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+													
+												@endif
+											@else	
+												<i class="fas fa-sort float-right mt-1"></i>
+											@endif
+	    								</th>
+	    								<th class="cursor-pointer px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider" wire:click="order('title')">
+	    									Título
+
+											@if ($sort == 'title')
+												@if ($direction == 'asc')
+													<i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+												@else
+													<i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+													
+												@endif
+											@else	
+												<i class="fas fa-sort float-right mt-1"></i>
+											@endif
+
+	    								</th>
+	    								<th class="cursor-pointer px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider" wire:click="order('content')">
+	    									Contenido
+
+											@if ($sort == 'content')
+												@if ($direction == 'asc')
+													<i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+												@else
+													<i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+													
+												@endif
+											@else	
+												<i class="fas fa-sort float-right mt-1"></i>
+											@endif
+	    								</th>
+	    							</tr>
+	    						</thead>
+								@foreach ($posts as $post)
+		    						<tbody>
+		    							<tr>
+		    								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+		    									<p class="text-gray-900">{{$post->id}}</p>
+		    								</td>
+		    								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+		    									<p class="text-gray-900">{{$post->title}}</p>
+		    								</td>
+		    								<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+		    									<p class="text-gray-900">
+		    										{{$post->content}}
+		    									</p>
+		    								</td>
+		    							</tr>    							
+		    						</tbody>
+								@endforeach
+	    					</table>
+
+						@else
+							<div class="px-6 py-4">
+								No existe ningún registro que coincida
+							</div>
+						@endif
+    					
+    					
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</div>
